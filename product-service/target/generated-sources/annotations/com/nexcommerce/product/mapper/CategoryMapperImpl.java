05ccf8cace2838c1cb96119967213a23e9176d3c
luxwxin <xiewenxin.xwx@alibaba-inc.com>
2025-03-26 14:26:27 +0800

    fix: 展示注释有编译问题的模块

--- product-service/target/generated-sources/annotations/com/nexcommerce/product/mapper/CategoryMapperImpl.java
+++ product-service/target/generated-sources/annotations/com/nexcommerce/product/mapper/CategoryMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-26T14:12:51+0800",
+    date = "2025-03-26T14:25:56+0800",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Homebrew)"
 )
 @Component
